70a57c60d58ebe2b51bcc678cf65da39487a8eed
vitaliy <vitaliy-eroshin@yandex.ru>
2022-03-23 20:21:37 +0300

    Added buttons for maze state control

--- src/main.py
+++ src/main.py
@@ -15,21 +15,36 @@ class MazeGenerator():
         self.setup_window()
 
         self.it = self.generator.generate()
-        self.go()
         self.root.mainloop()
 
+    def make_button(self, frame, text, command):
+        return tk.Button(
+            frame,
+            text=text,
+            command=command,
+            highlightbackground="#242424",
+            height=1,
+            width=7
+        )
 
     def setup_window(self):
         self.root = tk.Tk()
         self.root.title("Maze generator")
+        self.root.configure(background="#242424")
         self.canvas = tk.Canvas(
             self.root, 
             height=self.window_height, 
             width=self.window_width,
-            background="#333333"
+            background="#333333",
+            highlightbackground="black"
         )
-        self.canvas.pack()
+        self.canvas.pack(side="right")
+        self.frame = tk.Frame(self.root, padx=10, pady=10, background="#242424")
+        self.make_button(self.frame, "Run", self.start).pack(side="top")
+        self.make_button(self.frame, "Stop", self.stop).pack(side="top")
+        self.make_button(self.frame, "Reset", self.reset).pack(side="top")
 
+        self.frame.pack(side="top")
 
     def draw_cell(self, x, y):
         self.canvas.create_rectangle(
@@ -38,18 +53,29 @@ class MazeGenerator():
             fill="#a3fff6"
         )
 
+    def start(self):
+        self.running = True
+        self.go()
+
+    def stop(self):
+        self.running = False
+
+    def reset(self):
+        self.it = self.generator.generate()
+        self.generator.clear()
+        self.canvas.delete('all')
 
     def go(self):
+        if not self.running:
+            return
+
         try:
             pos = next(self.it)
             self.draw_cell(pos[1] * self.scale, pos[0] * self.scale)
             self.canvas.update()
             self.root.after(5, self.go)
         except StopIteration:
-            self.it = self.generator.generate()
-            self.generator.clear()
-            self.canvas.delete('all')
-            self.root.after(5, self.go)
+            self.running = False
 
 
 g = MazeGenerator(640, 480, 16)
