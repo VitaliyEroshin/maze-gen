2423399097c0a15ecbb4b7ece62aecdc916ec35f
vitaliy <vitaliy-eroshin@yandex.ru>
2022-03-23 18:39:03 +0300

    Added Generator base class

---
+++ src/generation/generator.py
@@ -0,0 +1,29 @@
+import importlib
+class Generator():
+    def __init__(self, width, height):
+        self.load_algorithm("dfs")
+        self.resize(width, height)
+
+    def resize(self, width, height):
+        self.width = width
+        self.height = height
+        self.maze = [[False] * (width // 2) for _ in range(height // 2)]
+        self.algo.resize(width, height)
+
+    def load_algorithm(self, path):
+        try:
+            self.algo = importlib.import_module("algorithms." + path)
+            
+        except ImportError:
+            print("No module found")
+
+    def generate(self):
+        for x in self.algo.algorithm(self.maze):
+            yield x
+
+    def clear(self):
+        for i in range(self.height // 2):
+            for j in range(self.width // 2):
+                self.maze[i][j] = False
+
+    
