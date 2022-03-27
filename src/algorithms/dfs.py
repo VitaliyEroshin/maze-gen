99988935cfb1948471d5459fe88d14716430b081
vitaliy <vitaliy-eroshin@yandex.ru>
2022-03-27 18:17:52 +0300

    Now DFS is stack based

--- src/algorithms/dfs.py
+++ src/algorithms/dfs.py
@@ -1,6 +1,5 @@
 import random
 
-
 def correct(x, y):
     return x < correct.height and x >= 0 and y < correct.width and y >= 0
 
@@ -10,21 +9,34 @@ def resize(width, height):
     correct.height = height
 
 
-def algorithm(maze, x=0, y=0, seed=0):
-    maze[x // 2][y // 2] = True
-    yield [x, y]
+def algorithm(maze):
+    stack = []
+    stack.append([0, 0])
+    while stack != []:
+        pos = stack[-1]
+        yield pos
+        x = pos[0]
+        y = pos[1]
 
-    delta = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+        maze[x // 2][y // 2] = True
 
-    # random.Random(seed).shuffle(delta)
+        delta = [[0, -1], [0, 1], [1, 0], [-1, 0]]
 
-    random.shuffle(delta)
-    for d in delta:
-        new_x = x + 2 * d[0]
-        new_y = y + 2 * d[1]
+        random.shuffle(delta)
+        
+        processed = True
 
-        if correct(new_x // 2, new_y // 2):
-            if not maze[new_x // 2][new_y // 2]:
-                yield [x + d[0], y + d[1]]
-                for t in algorithm(maze, x=new_x, y=new_y):
-                    yield t
+        for d in delta:
+            new_x = x + 2 * d[0]
+            new_y = y + 2 * d[1]
+
+            if correct(new_x // 2, new_y // 2):
+                if not maze[new_x // 2][new_y // 2]:
+                    yield [x + d[0], y + d[1]]
+                    stack.append([new_x, new_y])
+                    processed = False
+                    break
+            
+        if processed:
+            stack.pop()
+                    
